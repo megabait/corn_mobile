aa3f80ccaa6a0dae487260ebdd05f98caa9e29e0
pavel <info@megabait.kz>
2020-05-01 16:28:47 +0600

    first corn

---
+++ corn_mobile/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using corn_mobile.Services;
+using corn_mobile.Views;
+
+namespace corn_mobile
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
